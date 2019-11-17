f2e4810093eaa07a8f8ffc16690a4af356db27c1
zJtmoon <850886237@qq.com>
2019-11-17 21:05:56 +0800

    添加一个消息监听类

---
+++ src/main/java/com/example/rabbitmq/config/PaymentNotifySender.java
@@ -0,0 +1,20 @@
+package com.example.rabbitmq.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zjt
+ * @desc： 添加一个消息发送类（生产者）  将消息发送至默认的交换机且routingKey为notify.payment
+ * @Date create in 2019-11-17
+ */
+@Component
+public class PaymentNotifySender {
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void sender(String msg){
+        rabbitTemplate.convertAndSend("notify.payment", msg);
+    }
+}
