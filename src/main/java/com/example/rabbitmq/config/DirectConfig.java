f08ca0b585db9fd64af9caa8021878774bf69ea2
zJtmoon <850886237@qq.com>
2019-11-17 21:04:01 +0800

    消息的发送者

---
+++ src/main/java/com/example/rabbitmq/config/DirectConfig.java
@@ -0,0 +1,21 @@
+package com.example.rabbitmq.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author zjt
+ * @desc：添加一个配置类（消费者）
+ * 配置一个routingKey为notify.payment的消息队列
+ * @Date create in 2019-11-17
+ */
+@Configuration
+public class DirectConfig {
+    @Bean
+    public Queue paymentNotifyQueue() {
+        return new Queue("notify.payment");
+    }
+}
+
